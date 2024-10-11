9279c4617d74437c04d0f8a9f0bfd7e5a67a9f22
Meya <gabzmejia117@gmail.com>
2024-10-11 14:32:15 +0800

    initial commit

--- bot.py
+++ bot.py
@@ -1,13 +1,26 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
 
-# Create bot instance
-bot = commands.Bot(command_prefix="!")
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+# Create an instance of Intents
+intents = discord.Intents.all()
+intents.messages = True  # Allow the bot to receive messages
+intents.guilds = True    # Allow the bot to receive guild events
+intents.voice_states = True  # Allow the bot to receive voice state updates
+
+# Create a bot instance with the specified intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Define command 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Hello, {0.author}!".format(ctx))
+    await ctx.send(f"Hello Sir, {ctx.author}!")
 
-# run bot
-bot.run("YMTI5NDE2MjU4ODAzMjE3NjEzOA.GSgcAv.hFjHIZShOGkQLifCfwXKtbGI_mc3pkqJCTgbkA")
+# Run bot
+bot.run(TOKEN)
